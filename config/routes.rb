1be62d268f794df23a862db5ed4dee5b668effe1
ferso <fischer.ferso@gmail.com>
2022-10-07 23:02:15 +0200

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 # GET /about
-get "/about", to: "about#index"
-root          to: "main#index"
+get "/about-us", to: "about#index", as: :about
+get "/",         to: "main#index"
 
 root 'application#hello'
 
